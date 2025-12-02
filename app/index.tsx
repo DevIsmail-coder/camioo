d066c47b783cf09d29f99a8f18bb0ca8a22e94c6
DevIsmail-coder <ismailolawale777@gmail.com>
2025-12-02 22:15:12 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const index = () => {
+  return (
+  <View className='bg-green-300'>
+    <Text className='text-red-400'>hello world</Text>
+    <Text className='text-blue-500'>welcome ismail</Text>
+  </View>
+  )
+}
+
+export default index
